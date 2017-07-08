95180de57ac0d7374d46927615049c50fd47b28c
poz2k4444 <poz2k4444@gmail.com>
2017-07-08 15:05:11 -0500

    Beautify of abp::legacy::webserver class

--- modules/adblockplus/manifests/legacy/webserver.pp
+++ modules/adblockplus/manifests/legacy/webserver.pp
@@ -1,36 +1,48 @@
+# == Class: adblockplus::legacy::webserver
+#
+# A container for migrating obsolete resources in web2, formerly located
+# in manifests/webserver.pp.
+#
+# See http://hub.eyeo.com/issues/2007 for more information.
+#
 class adblockplus::legacy::webserver {
-  $sitescripts_var_dir = '/var/lib/sitescripts'
-  $subscriptions_repo = "${sitescripts_var_dir}/subscriptionlist"
+  $subscription_repo = '/home/www/subscriptionlist'
 
-  user {'sitescripts':
-    ensure => present,
-    home => $sitescripts_var_dir
-  }
-
-  file {$sitescripts_var_dir:
-    ensure => directory,
-    mode => 0755,
-    owner => 'sitescripts',
-    group => 'sitescripts'
-  }
+  $fetch_repo_cmd = [
+    'hg', 'clone',
+    '--noupdate',
+    'https://hg.adblockplus.org/subscriptionlist',
+    $subscription_repo,
+  ]
 
   exec {'fetch_repository_subscriptionlist':
-    command => "hg clone --noupdate https://hg.adblockplus.org/subscriptionlist $subscriptions_repo",
+    command => shellquote($fetch_repo_cmd),
     path => '/usr/local/bin:/usr/bin:/bin',
-    user => 'sitescripts',
+    user => 'www',
     timeout => 0,
-    onlyif => "test ! -d $subscriptions_repo",
-    require => [Package['mercurial'], File[$sitescripts_var_dir]]
+    onlyif => "test ! -d $subscription_repo",
+    require => Package['mercurial'],
   }
+
+  $update_repo_cmd = [
+    'hg', 'pull',
+    '--quiet', '--repository',
+    $subscription_repo,
+  ]
 
   cron {'update_repository_subscriptionlist':
     ensure => present,
     environment => hiera('cron::environment', []),
-    command => "hg pull --quiet --repository $subscriptions_repo",
-    user => 'sitescripts',
+    command => shellquote($update_repo_cmd),
+    user => 'www',
     minute => '1-59/20',
     require => Exec['fetch_repository_subscriptionlist']
   }
+
+  $generate_docs_cmd = [
+    'python', '-m',
+    'sitescripts.docs.bin.generate_docs',
+  ]
 
   cron {'generate_docs':
     ensure => 'present',
@@ -40,9 +52,8 @@ class adblockplus::legacy::webserver {
       Package['make', 'doxygen'],
       File['/var/www/docs'],
     ],
-    command => 'python -m sitescripts.docs.bin.generate_docs',
+    command => shellquote($generate_docs_cmd),
     user => www,
     minute => '5-55/10',
   }
-
 }
