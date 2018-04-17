0d1c0b7e555ca21b458ee415d62011ba4927b85a
poz2k4444 <poz2k4444@gmail.com>
2018-04-17 14:52:02 -0500

    Add fancy example

--- modules/adblockplus/manifests/web/static/hook.pp
+++ modules/adblockplus/manifests/web/static/hook.pp
@@ -22,6 +22,13 @@
 #    },
 #  }
 #
+#  adblockplus::web::static::hook {'uptime':
+#    'file' => {
+#      target: '/usr/bin/uptime',
+#      ensure: 'link',
+#    },
+#  }
+#
 define adblockplus::web::static::hook (
   $file = {},
 ) {
@@ -30,10 +37,10 @@ define adblockplus::web::static::hook (
 
   ensure_resource('file', "web-deploy-hook#${title}", merge({
     group => $adblockplus::web::static::deploy_user,
+    ensure => ensure_file_state($adblockplus::web::static::ensure),
   }, $file, {
     mode => '0755',
     owner => $adblockplus::web::static::deploy_user,
-    ensure => ensure_file_state($adblockplus::web::static::ensure),
     path => $hook_path,
   }))
 }
