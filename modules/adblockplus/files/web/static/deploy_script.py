83eb85bc5cdea1105abae1680dad9b278120aa5e
poz2k4444 <poz2k4444@gmail.com>
2018-06-05 19:27:07 -0500

    All changes

--- modules/adblockplus/files/web/static/deploy_script.py
+++ modules/adblockplus/files/web/static/deploy_script.py
@@ -9,23 +9,14 @@ import shutil
 import tarfile
 import tempfile
 import urllib
-import urllib2
-
-_tmp_dir = tempfile.mkdtemp()
 
 
-def download(url):
+def download(url, tmp_dir):
     file_name = url.split('/')[-1]
-    abs_file_name = os.path.join(_tmp_dir, file_name)
+    abs_file_name = os.path.join(tmp_dir, file_name)
     print 'Downloading: ' + file_name
-    try:
-        urllib.urlretrieve(url, abs_file_name)
-        return abs_file_name
-    except urllib2.HTTPError as e:
-        if e.code == 404:
-            sys.exit('File not found on remote source')
-    except Exception as e:
-        sys.exit(e)
+    urllib.urlretrieve(url, abs_file_name)
+    return abs_file_name
 
 
 def calculate_md5(file):
@@ -41,10 +32,10 @@ def read_md5(file):
     return md5_result.strip()
 
 
-def untar(tar_file):
+def untar(tar_file, tmp_dir):
     if tarfile.is_tarfile(tar_file):
         with tarfile.open(tar_file, 'r:gz') as tar:
-            tar.extractall(_tmp_dir)
+            tar.extractall(tmp_dir)
 
 
 def remove_tree(to_remove):
@@ -59,7 +50,7 @@ def deploy_files(dcmp):
     for name in dcmp.diff_files:
         copytree(dcmp.right, dcmp.left)
     for name in dcmp.left_only:
-        remove_tree(dcmp.left + "/" + name)
+        remove_tree(os.path.join(dcmp.left + "/" + name))
     for name in dcmp.right_only:
         copytree(dcmp.right, dcmp.left)
     for sub_dcmp in dcmp.subdirs.values():
@@ -91,31 +82,32 @@ if __name__ == '__main__':
                https://helpcenter.eyeofiles.com""",
     )
     parser.add_argument('--hash', action='store', type=str,
-                        nargs='?', required=True,
+                        required=True,
                         help='Hash of the commit to deploy')
     parser.add_argument('--source', action='store', type=str,
-                        required=True, nargs='?',
+                        required=True,
                         help='The source where files will be downloaded')
-    parser.add_argument('--website', action='store', type=str, nargs='?',
+    parser.add_argument('--website', action='store', type=str,
                         help='The name of the website [e.g. help.eyeo.com]')
     args = parser.parse_args()
     hash = args.hash
     source = args.source
     url_file = '{0}/{1}.tar.gz'.format(source, hash)
     url_md5 = '{0}/{1}.md5'.format(source, hash)
-    down_file = download(url_file)
-    down_md5 = download(url_md5)
+    tmp_dir = tempfile.mkdtemp()
     try:
+        down_file = download(url_file, tmp_dir)
+        down_md5 = download(url_md5, tmp_dir)
         if calculate_md5(down_file) == read_md5(down_md5):
-            untar(down_file)
-            hash_directory = os.path.join(_tmp_dir, hash)
-            destination = '/var/www/' + args.website
+            untar(down_file, tmp_dir)
+            hash_directory = os.path.join(tmp_dir, hash)
+            destination = os.path.join('/var/www/', args.website)
             dcmp = dircmp(destination, hash_directory)
-            print "Deploying files"
+            print 'Deploying files'
             deploy_files(dcmp)
         else:
             sys.exit("Hashes don't match")
     except Exception as e:
         sys.exit(e)
     finally:
-        shutil.rmtree(_tmp_dir)
+        shutil.rmtree(tmp_dir)
